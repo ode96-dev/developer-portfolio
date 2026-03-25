0a0abb063fa4e57b2a3b815d525c55dd9e5b1641
ode96-dev <150020491+ode96-dev@users.noreply.github.com>
2026-03-25 18:05:47 +0300

    hero-section radial-background fix

--- src/backgrounds/radial-gradient-background.tsx
+++ src/backgrounds/radial-gradient-background.tsx
@@ -1,7 +1,101 @@
 import React from "react";
 
-const RadialGradientBackground = ({ variant }: { variant: string }) => {
-  return <div></div>;
+const RadialGradientBackground = ({
+  variant,
+  gradients,
+}: {
+  variant: string;
+  gradients: [];
+}) => {
+  const variants = {
+    hero: [
+      {
+        position: "top-1 left-1 -translate-x-1/2 -translate-y-1/2",
+        size: "w-[1400px] h-[1400px]",
+        colors: [
+          { color: "rgba(167, 120, 255, 0.25)", stop: "100%" },
+          { color: "rgba(167, 120, 255, 0.45)", stop: "100%" },
+          { color: "rgba(167, 120, 255, 0.5)", stop: "100%" },
+          { color: "rgba(167, 120, 255, 0.45)", stop: "100%" },
+          { color: "rgba(167, 120, 255, 0.25)", stop: "100%" },
+        ],
+        blur: "0px",
+        opacity: 0.5,
+      },
+      {
+        position: "top-1 left-1",
+        size: "w-[1400px] h-[1400px]",
+        colors: [
+          { color: "rgba(167, 120, 255, 0.25)", stop: "100%" },
+          { color: "rgba(167, 120, 255, 0.45)", stop: "100%" },
+          { color: "rgba(167, 120, 255, 0.5)", stop: "100%" },
+          { color: "rgba(167, 120, 255, 0.45)", stop: "100%" },
+          { color: "rgba(167, 120, 255, 0.25)", stop: "100%" },
+        ],
+        blur: "0px",
+        opacity: 0.5,
+      },
+      {
+        position: "bottom-1 right-1",
+        size: "w-[1400px] h-[1400px]",
+        colors: [
+          { color: "rgba(167, 120, 255, 0.25)", stop: "100%" },
+          { color: "rgba(167, 120, 255, 0.45)", stop: "100%" },
+          { color: "rgba(167, 120, 255, 0.5)", stop: "100%" },
+          { color: "rgba(167, 120, 255, 0.45)", stop: "100%" },
+          { color: "rgba(167, 120, 255, 0.25)", stop: "100%" },
+        ],
+        blur: "0px",
+        opacity: 0.5,
+      },
+    ],
+    about: [
+      {
+        position: "bottom-0 left-[75%]",
+        size: "w-[700px] h-[700px]",
+        colors: [
+          { color: "rgba(167, 120, 255, 0.25)", stop: "100%" },
+          { color: "rgba(167, 120, 255, 0.45)", stop: "100%" },
+          { color: "rgba(167, 120, 255, 0.5)", stop: "100%" },
+          { color: "rgba(167, 120, 255, 0.45)", stop: "100%" },
+          { color: "rgba(167, 120, 255, 0.25)", stop: "100%" },
+        ],
+        blur: "0px",
+        opacity: 0.5,
+      },
+    ],
+  };
+
+  const activeGradients =
+    variant === "custom" ? gradients : variants[variant] || variants.hero;
+
+  const generateGradient = (
+    colors: {
+      color: string;
+      stop: string;
+    }[],
+  ) => {
+    const colorStops = colors
+      .map(({ color, stop }) => `${color} ${stop}`)
+      .join(", ");
+    return `radial-gradient(circle at center, transparent 0%, transparent 30%, ${colorStops}, transparent 60%, transparent 100%)`;
+  };
+
+  return (
+    <div className="">
+      {activeGradients.map((gradient, index) => (
+        <div
+          key={index}
+          className={`absolute ${gradient.position} ${gradient.size} rounded-full`}
+          style={{
+            background: generateGradient(gradient.colors),
+            filter: `blur(${gradient.blur})`,
+            opacity: gradient.opacity,
+          }}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default RadialGradientBackground;
